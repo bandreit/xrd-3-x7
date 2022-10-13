c4f693b1ef9f35ab934d3c00b72e2b589b91473b
EdvinasAndrijauskas <edvinas.andrijauskas2000@gmail.com>
2022-10-13 11:47:44 +0200

    Loggin on trigger value

---
+++ Assets/RightHandTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RightHandTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private InputActionProperty triggered;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = triggered.action.ReadValue<float>();
+        
+        Debug.Log(triggerValue);
+    }
+}
